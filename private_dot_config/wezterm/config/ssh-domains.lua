c5fd5c8484a0932a30da4147002867023c896b11
wzekin <wzekin@gmail.com>
2023-10-26 17:19:31 +0800

    Update .config/wezterm/config/ssh-domains.lua

--- private_dot_config/wezterm/config/ssh-domains.lua
+++ private_dot_config/wezterm/config/ssh-domains.lua
@@ -3,16 +3,10 @@ local wezterm = require("wezterm")
 local ssh_domains = {}
 
 for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
-   local multiplexing
-   if host == "zekin" then
-      multiplexing = "WezTerm"
-   else
-      multiplexing = "None"
-   end
    table.insert(ssh_domains, {
       name = host,
       remote_address = config.hostname,
-      multiplexing = multiplexing,
+      multiplexing = "None",
       assume_shell = "Posix",
       default_prog = { "zsh" },
    })
