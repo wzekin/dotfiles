7e799eedfe35c71b436887c70e4f0771eab3739e
wangzekun.zekin <wangzekun.zekin@bytedance.com>
2024-05-12 16:33:57 +0800

    Add .config/wezterm/colors/nordfox.toml Add .config/wezterm/keybinds.lua Add .config/wezterm/on.lua Add .config/wezterm/utils.lua Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,106 +1,223 @@
 local wezterm = require("wezterm")
-local custom = require("colors.custom")
-local keybindings = require("config.key-bindings")
-local launch_menu = require("config.launch-menu")
-local ssh_domains = require("config.ssh-domains")
-local shell = require("config.shell")
-require("config.right-status").setup()
-require("config.notify").setup()
-require("config.tab-title").setup()
+local utils = require("utils")
+local keybinds = require("keybinds")
+local scheme = wezterm.get_builtin_color_schemes()["nord"]
+local gpus = wezterm.gui.enumerate_gpus()
+require("on")
 
-local font_primary = "Cascadia Code"
+-- /etc/ssh/sshd_config
+-- AcceptEnv TERM_PROGRAM_VERSION COLORTERM TERM TERM_PROGRAM WEZTERM_REMOTE_PANE
+-- sudo systemctl reload sshd
 
-local function font(name, params)
-   return wezterm.font(name, params)
+---------------------------------------------------------------
+--- functions
+---------------------------------------------------------------
+-- selene: allow(unused_variable)
+---@diagnostic disable-next-line: unused-function, unused-local
+local function enable_wayland()
+	local session = os.getenv("DESKTOP_SESSION")
+	if session == "hyprland" then
+		return true
+	end
+	-- local wayland = os.getenv("XDG_SESSION_TYPE")
+	-- if wayland == "wayland" then
+	-- 	return true
+	-- end
+	return false
 end
 
--- [[ font with fallback ]]
--- local function font_with_fallback(name, params)
---   local names = { name }
---   return wezterm.font_with_fallback(names, params)
--- end
+---------------------------------------------------------------
+--- Merge the Config
+---------------------------------------------------------------
+local function create_ssh_domain_from_ssh_config(ssh_domains)
+	if ssh_domains == nil then
+		ssh_domains = {}
+	end
+	for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
+		table.insert(ssh_domains, {
+			name = host,
+			remote_address = config.hostname .. ":" .. config.port,
+			username = config.user,
+			multiplexing = "None",
+			assume_shell = "Posix",
+		})
+	end
+	return { ssh_domains = ssh_domains }
+end
 
-return {
-   -- fonts
-   font = font(font_primary),
-   font_size = 13,
+--- load local_config
+-- Write settings you don't want to make public, such as ssh_domains
+package.path = os.getenv("HOME") .. "/.local/share/wezterm/?.lua;" .. package.path
+local function load_local_config(module)
+	local m = package.searchpath(module, package.path)
+	if m == nil then
+		return {}
+	end
+	return dofile(m)
+	-- local ok, _ = pcall(require, "local")
+	-- if not ok then
+	-- 	return {}
+	-- end
+	-- return require("local")
+end
 
-   -- colour scheme
-   -- colors = custom,
-   color_scheme = "Tokyo Night (Gogh)",
+local local_config = load_local_config("local")
 
-   -- background
-   background = {
-      {
-         source = { File = wezterm.config_dir .. "/backdrops/bg3.png" },
-      },
-      {
-         source = { Color = custom.background },
-         height = "100%",
-         width = "100%",
-         opacity = 0.90,
-      },
-   },
+-- local local_config = {
+-- 	ssh_domains = {
+-- 		{
+-- 			-- This name identifies the domain
+-- 			name = "my.server",
+-- 			-- The address to connect to
+-- 			remote_address = "192.168.8.31",
+-- 			-- The username to use on the remote host
+-- 			username = "katayama",
+-- 		},
+-- 	},
+-- }
+-- return local_config
 
-   -- scroll bar
-   enable_scroll_bar = true,
-
-   -- status
-   status_update_interval = 1000,
-
-   -- tab bar
-   enable_tab_bar = true,
-   hide_tab_bar_if_only_one_tab = false,
-   use_fancy_tab_bar = false,
-   tab_max_width = 25,
-   show_tab_index_in_tab_bar = false,
-   switch_to_last_active_tab_when_closing_tab = true,
-
-   -- window
-   window_padding = {
-      left = 5,
-      right = 10,
-      top = 12,
-      bottom = 7,
-   },
-   window_close_confirmation = "NeverPrompt",
-   window_frame = {
-      active_titlebar_bg = "#090909",
-      font = font(font_primary, { bold = true }),
-      font_size = 12,
-   },
-   automatically_reload_config = true,
-   inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 },
-   -- window_background_opacity = 0.9,
-
-   -- keybindings
-   disable_default_key_bindings = true,
-   keys = keybindings,
-
-   -- mousebindings
-   mouse_bindings = {
-      -- Ctrl-click will open the link under the mouse cursor
-      {
-         event = { Up = { streak = 1, button = "Left" } },
-         mods = "CTRL",
-         action = wezterm.action.OpenLinkAtMouseCursor,
-      },
-   },
-
-   -- shells
-   default_prog = shell,
-   launch_menu = launch_menu,
-
-   -- ssh
-   ssh_domains = ssh_domains,
-
-   wsl_domains = {
-      {
-         name = "WSL:Arch",
-         distribution = "Arch",
-         username = "zekin",
-         default_cwd = "/home/zekin",
-         default_prog = { "zsh" },
-      },
-   },
+---------------------------------------------------------------
+--- Config
+---------------------------------------------------------------
+local config = {
+	-- font = wezterm.font("Cica"),
+	-- font_size = 10.0,
+	font = wezterm.font("Cascadia Code"),
+	font_size = 16,
+	-- cell_width = 1.1,
+	-- line_height = 1.1,
+	-- font_rules = {
+	-- 	{
+	-- 		italic = true,
+	-- 		font = wezterm.font("Cica", { italic = true }),
+	-- 	},
+	-- 	{
+	-- 		italic = true,
+	-- 		intensity = "Bold",
+	-- 		font = wezterm.font("Cica", { weight = "Bold", italic = true }),
+	-- 	},
+	-- },
+	check_for_updates = false,
+	use_ime = true,
+	ime_preedit_rendering = "Builtin",
+	use_dead_keys = false,
+	warn_about_missing_glyphs = false,
+	-- enable_kitty_graphics = false,
+	animation_fps = 1,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	cursor_blink_rate = 0,
+	enable_wayland = enable_wayland(),
+	-- https://github.com/wez/wezterm/issues/1772
+	-- enable_wayland = true,
+	color_scheme = "Catppuccin Macchiato",
+	color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" },
+	hide_tab_bar_if_only_one_tab = false,
+	adjust_window_size_when_changing_font_size = false,
+	selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	use_fancy_tab_bar = false,
+	colors = {
+		tab_bar = {
+			background = scheme.background,
+			new_tab = { bg_color = "#2e3440", fg_color = scheme.ansi[8], intensity = "Bold" },
+			new_tab_hover = { bg_color = scheme.ansi[1], fg_color = scheme.brights[8], intensity = "Bold" },
+			-- format-tab-title
+			-- active_tab = { bg_color = "#121212", fg_color = "#FCE8C3" },
+			-- inactive_tab = { bg_color = scheme.background, fg_color = "#FCE8C3" },
+			-- inactive_tab_hover = { bg_color = scheme.ansi[1], fg_color = "#FCE8C3" },
+		},
+	},
+	exit_behavior = "CloseOnCleanExit",
+	tab_bar_at_bottom = false,
+	window_close_confirmation = "AlwaysPrompt",
+	-- window_background_opacity = 0.8,
+	disable_default_key_bindings = true,
+	-- visual_bell = {
+	-- 	fade_in_function = "EaseIn",
+	-- 	fade_in_duration_ms = 150,
+	-- 	fade_out_function = "EaseOut",
+	-- 	fade_out_duration_ms = 150,
+	-- },
+	-- separate <Tab> <C-i>
+	enable_csi_u_key_encoding = true,
+	leader = { key = "Space", mods = "CTRL|SHIFT" },
+	keys = keybinds.create_keybinds(),
+	key_tables = keybinds.key_tables,
+	mouse_bindings = keybinds.mouse_bindings,
+	-- https://github.com/wez/wezterm/issues/2756
+	webgpu_preferred_adapter = gpus[1],
+	front_end = "OpenGL",
 }
+
+for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
+	if gpu.backend == "Vulkan" and gpu.device_type == "IntegratedGpu" then
+		config.webgpu_preferred_adapter = gpu
+		config.front_end = "WebGpu"
+		break
+	end
+end
+
+config.hyperlink_rules = {
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		-- Before
+		--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+		--format = '$0',
+		-- After
+		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
+	{
+		regex = [[["]?(?:([\w\d\.]+):)?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+		format = "https://$1/$2/$4",
+		highlight = 1,
+	},
+}
+table.insert(config.hyperlink_rules, {
+	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://github.com/$1/$3",
+})
+table.insert(config.hyperlink_rules, {
+	regex = [[go/pkg/mod/([\w\d\.]+)(/){1}([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)]],
+	format = "https://$1/$3/$5",
+})
+
+local merged_config = utils.merge_tables(config, local_config)
+return utils.merge_tables(merged_config, create_ssh_domain_from_ssh_config(merged_config.ssh_domains))
