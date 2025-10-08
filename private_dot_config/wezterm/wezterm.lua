2128a8b7715b60f8fc9dcbb7a817b0dd73008bd3
wangzekun.zekin <wangzekun.zekin@bytedance.com>
2025-10-08 10:12:30 +0800

    Update .config/wezterm/background.lua Add .config/wezterm/check_bytedance_mac.sh Add .config/wezterm/antigen.zsh Remove .config/wezterm/keybinds.lua Remove .config/wezterm/on.lua Remove .config/wezterm/utils.lua Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,94 +1,8 @@
 local wezterm = require("wezterm")
-local utils = require("utils")
-local keybinds = require("keybinds")
 local background = require("background")
 local scheme = wezterm.get_builtin_color_schemes()["nord"]
 local gpus = wezterm.gui.enumerate_gpus()
-require("on")
 
--- /etc/ssh/sshd_config
--- AcceptEnv TERM_PROGRAM_VERSION COLORTERM TERM TERM_PROGRAM WEZTERM_REMOTE_PANE
--- sudo systemctl reload sshd
-
-local is_linux = function()
-	return wezterm.target_triple:find("linux") ~= nil
-end
-
-local is_darwin = function()
-	return wezterm.target_triple:find("darwin") ~= nil
-end
-
-local is_win = function()
-	return wezterm.target_triple:find("windows") ~= nil
-end
-
--- Reload the configuration every hour to upload backgrounds
-local time = require("math").random(3000, 7000)
-wezterm.time.call_after(time, function()
-	wezterm.reload_configuration()
-end)
-
----------------------------------------------------------------
---- functions
----------------------------------------------------------------
--- selene: allow(unused_variable)
----@diagnostic disable-next-line: unused-function, unused-local
-local function enable_wayland()
-	local session = os.getenv("DESKTOP_SESSION")
-	if session == "hyprland" then
-		return true
-	end
-	-- local wayland = os.getenv("XDG_SESSION_TYPE")
-	-- if wayland == "wayland" then
-	-- 	return true
-	-- end
-	return false
-end
-
----------------------------------------------------------------
---- Merge the Config
----------------------------------------------------------------
-local function create_ssh_domain_from_ssh_config(ssh_domains)
-	if ssh_domains == nil then
-		ssh_domains = {}
-	end
-	for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
-		table.insert(ssh_domains, {
-			name = host,
-			remote_address = config.hostname .. ":" .. config.port,
-			username = config.user,
-			multiplexing = "None",
-			assume_shell = "Posix",
-		})
-	end
-	return { ssh_domains = ssh_domains }
-end
-
----- load local_config
--- Write settings you don't want to make public, such as ssh_domains
-package.path = wezterm.home_dir .. "/.local/share/wezterm/?.lua;" .. package.path
-local function load_local_config(module)
-	local m = package.searchpath(module, package.path)
-	if m == nil then
-		return {}
-	end
-	return dofile(m)
-	-- local ok, _ = pcall(require, "local")
-	-- if not ok then
-	-- 	return {}
-	-- end
-	-- return require("local")
-end
-
-local local_config = load_local_config("local")
-
-local default_prog = nil
-
-if is_win() then
-	default_prog = {"nu.exe"}
-else
-	default_prog = {"zsh"}
-end
 ---------------------------------------------------------------
 --- Config
 ---------------------------------------------------------------
@@ -96,7 +10,6 @@ local config = {
 	-- font = wezterm.font("Cica"),
 	-- font_size = 10.0,
 	font = wezterm.font("Cascadia Code"),
-	default_prog = default_prog,
 	font_size = 15,
 	-- cell_width = 1.1,
 	-- line_height = 1.1,
@@ -121,7 +34,6 @@ local config = {
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
 	cursor_blink_rate = 0,
-	enable_wayland = enable_wayland(),
 	-- https://github.com/wez/wezterm/issues/1772
 	-- enable_wayland = true,
 	color_scheme = "Catppuccin Macchiato",
@@ -151,7 +63,6 @@ local config = {
 	tab_bar_at_bottom = false,
 	window_close_confirmation = "AlwaysPrompt",
 	-- window_background_opacity = 0.8,
-	disable_default_key_bindings = true,
 	-- visual_bell = {
 	-- 	fade_in_function = "EaseIn",
 	-- 	fade_in_duration_ms = 150,
@@ -159,11 +70,9 @@ local config = {
 	-- 	fade_out_duration_ms = 150,
 	-- },
 	-- separate <Tab> <C-i>
+
 	enable_csi_u_key_encoding = true,
 	leader = { key = "Space", mods = "CTRL|SHIFT" },
-	keys = keybinds.create_keybinds(),
-	key_tables = keybinds.key_tables,
-	mouse_bindings = keybinds.mouse_bindings,
 	-- https://github.com/wez/wezterm/issues/2756
 	webgpu_preferred_adapter = gpus[1],
 	front_end = "OpenGL",
@@ -171,68 +80,4 @@ local config = {
 	background = background.gif(),
 }
 
-for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
-	if gpu.backend == "Vulkan" and gpu.device_type == "IntegratedGpu" then
-		config.webgpu_preferred_adapter = gpu
-		config.front_end = "WebGpu"
-		break
-	end
-end
-
-config.hyperlink_rules = {
-	-- Matches: a URL in parens: (URL)
-	{
-		regex = "\\((\\w+://\\S+)\\)",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in brackets: [URL]
-	{
-		regex = "\\[(\\w+://\\S+)\\]",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in curly braces: {URL}
-	{
-		regex = "\\{(\\w+://\\S+)\\}",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in angle brackets: <URL>
-	{
-		regex = "<(\\w+://\\S+)>",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Then handle URLs not wrapped in brackets
-	{
-		-- Before
-		--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-		--format = '$0',
-		-- After
-		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-		format = "$1",
-		highlight = 1,
-	},
-	-- implicit mailto link
-	{
-		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-		format = "mailto:$0",
-	},
-	{
-		regex = [[["]?(?:([\w\d\.]+):)?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-		format = "https://$1/$2/$4",
-		highlight = 1,
-	},
-}
-table.insert(config.hyperlink_rules, {
-	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-	format = "https://github.com/$1/$3",
-})
-table.insert(config.hyperlink_rules, {
-	regex = [[go/pkg/mod/([\w\d\.]+)(/){1}([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)]],
-	format = "https://$1/$3/$5",
-})
-
-local merged_config = utils.merge_tables(config, local_config)
-return utils.merge_tables(merged_config, create_ssh_domain_from_ssh_config(merged_config.ssh_domains))
+return config
